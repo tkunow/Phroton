066e5dc6dae37764346b389b627fc602990683f8
timon.kunow@stu.haw-kiel.de <tkbuilder1900@gmail.com>
2026-08-03 10:44:26 +0200

    added tkinter theme for testing

--- source/frontend.py
+++ source/frontend.py
@@ -1,7 +1,19 @@
 import tkinter as tk
+from tkinter import ttk
 from PIL import Image, ImageTk
 from imageview import ImageView
 from custom_types import Rotation
+from os import path
+from pathlib import Path
+
+PROJECT_ROOT = Path(__file__).resolve().parent.parent
+ASSETS = path.join(PROJECT_ROOT, "assets")
+theme = "park"
+theme_path = path.join(ASSETS, theme.lower(), theme.lower() + ".tcl")
+
+class Theme:
+    DARK= "dark"
+    LIGHT= "light"
 
 class FrontEnd:
     def __init__(self, path: str):
@@ -11,6 +23,11 @@ class FrontEnd:
         self.tk_root.columnconfigure(0, weight=1)
         self.tk_root.rowconfigure(0, weight=1)
 
+        self.mode = Theme.DARK
+        print(theme_path)
+        self.tk_root.tk.call("source", theme_path)
+        self.tk_root.tk.call("set_theme", self.mode.lower())
+
         self.cv2_obj = ImageView()
 
         content = tk.Frame(self.tk_root)
@@ -19,20 +36,23 @@ class FrontEnd:
         content.columnconfigure(0, weight=1)
         content.rowconfigure(1, weight=1)
 
-        interactionbar = tk.Frame(content, padx=10, pady=10)
+        interactionbar = ttk.Frame(content, padding=10)
         interactionbar.grid(column=0, row=0, sticky="ew")
-        rleft = tk.Button(interactionbar, text="rleft", command=lambda: self.displayImage(self.cv2_obj.rotateImage(self.current_image, Rotation.LEFT)))
+        rleft = ttk.Button(interactionbar, text="rleft", command=lambda: self.displayImage(self.cv2_obj.rotateImage(self.current_image, Rotation.LEFT)))
         rleft.grid(column=0, row=0, padx=(0, 5))
-        rright = tk.Button(interactionbar, text="rright", command=lambda: self.displayImage(self.cv2_obj.rotateImage(self.current_image, Rotation.RIGHT)))
+        rright = ttk.Button(interactionbar, text="rright", command=lambda: self.displayImage(self.cv2_obj.rotateImage(self.current_image, Rotation.RIGHT)))
         rright.grid(column=1, row=0, padx=(0, 5))
 
-        self.panel = tk.Label(content, pady=10, padx=10)
+        self.panel = ttk.Label(content, padding=10)
         self.panel.grid(column=0, row=1)
 
         self.current_image = self.cv2_obj.readImage(path)
         self.displayImage(self.current_image)
 
-    def displayImage(self, image):
+    def changeTheme(self, theme: Theme) -> None:
+        self.mode = theme
+
+    def displayImage(self, image) -> None:
         self.current_image = image
         imgtk = ImageTk.PhotoImage(image=Image.fromarray(self.current_image))
         self.panel.imgtk = imgtk
--- source/main.py
+++ source/main.py
@@ -1,11 +1,26 @@
 from pathlib import Path
+from os import listdir, path
 from frontend import FrontEnd
 
 
 PROJECT_ROOT = Path(__file__).resolve().parent.parent
+WORKING_DIR = Path.cwd()
+
+# for testing 
 img_path = PROJECT_ROOT / "test" / "dots.bmp"
 
+ImageTypes = (".png", ".bmp", ".jpg")
+
+def listImages() -> List:
+    images = []
+    for items in listdir(WORKING_DIR):
+        if (items.endswith(ImageTypes)):
+            images.append(items)
+    return images
+
 if __name__ == "__main__":
+    print(listImages())
+
     phroton = FrontEnd(img_path)
     phroton.tk_root.mainloop()
 
